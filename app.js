da6f59ca886074a9e093cc4597c378ca07080bbe
Simone <simo.sprea1907@gmail.com>
2021-08-05 17:28:15 +0200

    crud api su libri nel db

---
+++ app.js
@@ -0,0 +1,156 @@
+//Inclusione dei moduli
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const porta = process.env.PORT || 5000
+
+//L'api risponde in formato json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Connessione database mysql
+const pool = mysql.createPool(
+    {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: 'toor',
+        database: 'MyBooks'
+    }
+);
+
+//Lista di tutti i libri nel db
+app.get('/libri', (req, res) => {
+    pool.getConnection((err, connection) => 
+        {
+            if(err)
+            {
+                throw err;
+            }
+
+            connection.query('SELECT * FROM Libri', (err, rows) => 
+            {
+                connection.release();
+
+                if(err)
+                {
+                    console.error(err);
+                }
+
+                res.send(rows);
+            })
+        }
+    )
+});
+
+//Lista di un particolare libro
+app.get('/libri/:id', (req, res) => {
+
+    pool.getConnection((err, connection) => 
+        {
+            if(err)
+            {
+                throw err;
+            }
+
+            connection.query('SELECT * FROM Libri where Isbn = ?', [req.params.id], (err, rows) => 
+            {
+                connection.release();
+
+                if(err)
+                {
+                    console.error(err);
+                }
+
+                res.send(rows);
+            })
+        }
+    )
+});
+
+//Elimina un libro dato l'isbn
+app.delete('/libri/:id', (req, res) => {
+
+    pool.getConnection((err, connection) => 
+        {
+            if(err)
+            {
+                throw err;
+            }
+
+            connection.query('DELETE FROM Libri where Isbn = ?', [req.params.id], (err, rows) => 
+            {
+                connection.release();
+
+                if(err)
+                {
+                    console.error(err);
+                }
+
+                res.send('Libro con isbn: ' + [req.params.id] + ' rimosso');
+            })
+        }
+    )
+});
+
+//Aggiunge un nuovo libro
+app.post('/libri', (req, res) => {
+
+    pool.getConnection((err, connection) => 
+        {
+            if(err)
+            {
+                throw err;
+            }
+            
+            const params = req.body     //prendo il nuovo oggetto libro dalla richiesta
+
+            connection.query('INSERT INTO Libri SET ?', params, (err, rows) => 
+            {
+                connection.release();
+
+                if(err)
+                {
+                    console.error(err);
+                }
+
+                res.send('Libro con isbn: ' + params.Isbn + ' aggiunto');
+            })
+        }
+    )
+});
+
+//Modifica di un libro
+app.put('/libri', (req, res) => {
+
+    pool.getConnection((err, connection) => 
+        {
+            if(err)
+            {
+                throw err;
+            }
+            
+            const { Isbn, Titolo, Autore, Pagine, PagineLette, Completato, Impressioni, Valutazione} = req.body;
+            
+            connection.query('UPDATE Libri SET Titolo = ?, Autore = ?, Pagine = ?, PagineLette = ?, Completato = ?, Impressioni = ?, Valutazione = ? WHERE Isbn = ?', [Titolo, Autore, Pagine, PagineLette, Completato, Impressioni, Valutazione, Isbn], 
+                (err, rows) => 
+            {
+                connection.release();
+
+                if(err)
+                {
+                    console.error(err);
+                }
+
+                res.send('Libro con isbn: ' + Isbn + ' modificato');
+            })
+        }
+    )
+});
+
+//App in ascolto
+app.listen(porta, () => console.info('In ascolto sulla porta ' + porta));
+
+
+
